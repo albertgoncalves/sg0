0bafd09a922767aae5a4c6c3d3fb67a8560c6bae
albertgoncalves <goncalves.albert@gmail.com>
2022-09-12 21:23:56 -0400

    fix profiler script

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -4,7 +4,6 @@ set -eu
 
 sudo sh -c "echo 1 > /proc/sys/kernel/perf_event_paranoid"
 sudo sh -c "echo 0 > /proc/sys/kernel/kptr_restrict"
-perf record --call-graph fp \
-    prime-run "$WD/bin/main" "$WD/src/vert.glsl" "$WD/src/frag.glsl"
+perf record --call-graph fp prime-run "$WD/bin/main"
 perf report
 rm perf.data*
