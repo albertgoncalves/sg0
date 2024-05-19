eba1fb98d562c8e50903e93b96eeddacb6e144b3
albertgoncalves <goncalves.albert@gmail.com>
2024-05-18 23:29:48 -0400

    bump llvm version

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 with import <nixpkgs> {};
-mkShell.override { stdenv = llvmPackages_16.stdenv; } {
+mkShell.override { stdenv = llvmPackages_17.stdenv; } {
     buildInputs = [
         feh
         gimp
